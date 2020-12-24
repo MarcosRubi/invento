9284a48d673f92422a0320518812b2a795aed79d
Daniel <danielhernandez9980@gmail.com>
2020-12-24 16:53:20 -0600

    Responsive menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-window.onload = ()=>{
-    
+function toggleMenu() {
+    const menu = document.querySelector('.menu')
+    const toggle = document.querySelector('.toggle')
+
+    menu.classList.toggle('active')
+    toggle.classList.toggle('active')
+}
+window.onload = () => {
+    window.addEventListener('scroll', () => {
+        const header = document.querySelector('header')
+        header.classList.toggle('sticky', scrollY > 0)
+    })
+
 }
