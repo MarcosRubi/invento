60281c42b617a2298a2d5a31b09900675d41ed23
Daniel <danielhernandez9980@gmail.com>
2020-12-25 18:36:40 -0600

    Banner responsive

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,24 @@ function toggleMenu() {
     menu.classList.toggle('active')
     toggle.classList.toggle('active')
 }
+function clearClass(elements) {
+    elements.forEach((element, index) => {
+        element.classList.remove('active')
+    })
+}
 window.onload = () => {
+
     window.addEventListener('scroll', () => {
         const header = document.querySelector('header')
         header.classList.toggle('sticky', scrollY > 0)
+    })
+
+    const nav_links = document.querySelectorAll('.menu a');
+    nav_links.forEach((element, index) => {
+        element.addEventListener('click', () => {
+            clearClass(nav_links);
+            element.classList.add('active')
+        })
     })
 
 }
